0f7b875162b4711a468437276a699232781d8e43
Sayantan Bera <sayantanbera25@gmail.com>
2025-09-04 15:50:05 +0530

    hero section update

--- src/components/AnimatedTextLines.jsx
+++ src/components/AnimatedTextLines.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 import React, { useRef } from "react";
 
